6b8a33eecb1f3661dcd7effbac14f2e00256bfa1
Dempsey Blum <dempsey@curiousit.com>
2020-09-23 13:11:08 +1000

    Finish minimax tree setup

--- src/MiniMax.java
+++ src/MiniMax.java
@@ -1,27 +1,55 @@
+import java.util.ArrayList;
 
 public class MiniMax {
 	// Builds a binary tree then performs a min max algorithm on it.
 	Tree tree;
+	int maxDepth;
 	
-	public void BuildTree(int depth, int[] leafValues) {
-		boolean isMaxNode = true;
+	public void BuildTree(int depth, ArrayList<Integer> leafValues) {
+		this.maxDepth = depth;
 		
 		Node rootNode = new Node(true);
 		
-		for (int i = 0; i < depth; i++) {
-			isMaxNode = !isMaxNode;
-			
-			for (int t = 0; t<(2^(depth-1)); t++) {
-				Node childOne = new Node(isMaxNode);
-				Node childTwo = new Node(isMaxNode);
-				
-				System.out.print(t);
-				
-				rootNode.setChildren(childOne, childTwo);
-			}
-		}
+		Node childOne = BuildTree(false, 1, leafValues);
+		Node childTwo = BuildTree(false, 1, leafValues);
+		
+		rootNode.setChildren(childOne, childTwo);
 		
 		tree = new Tree(rootNode);
 	}
+	
+	public Node BuildTree(boolean isMaxNode, int currDepth, ArrayList<Integer> leafValues) {
+		if (currDepth >= maxDepth) {
+			Node node = new Node(isMaxNode);
+			
+			node.setValue(leafValues.get(0));
+			
+			System.out.print(node.getValue());
+			
+			return node;
+		}
+		
+		// Create node and it's children.
+		Node node = new Node(isMaxNode);
+		
+		Node childOne = BuildTree(!isMaxNode, currDepth+1, leafValues);
+		Node childTwo = BuildTree(!isMaxNode, currDepth+1, leafValues);
+		
+		node.setChildren(childOne, childTwo);
+		
+		return node;
+		
+	}
+	
+	public void SerachTree() {
+		
+	}
 
+//	public boolean checkWin(Node node) {
+//		Node childOne = node.getChildOne();
+//		Node childTwo = node.getChildTwo();
+//		
+//		
+//	}
+	
 }
--- src/Setup.java
+++ src/Setup.java
@@ -1,17 +1,30 @@
+import java.util.ArrayList;
 
 public class Setup {
 	// NOTE: This only makes binary trees, where each node has 2 children or none if it's a leaf.
 	
-	int depth = 4; // Tempt value until user interface is finished, this includes the root.
-	int[] leafValues = {1,2,3,4,5,6,7,8}; // Make sure there are 2^(depth-1) values in this array
-	
-	double min;
-	double max;
-	
 	double alpha = Double.NEGATIVE_INFINITY;
 	double beta = Double.POSITIVE_INFINITY;
+
+	public static void main (String[] args) {
+		int depth = 4; // Tempt value until user interface is finished, this includes the root.
+		ArrayList LeafValues = new ArrayList<Integer>();
+		LeafValues.add(1);
+		LeafValues.add(2);
+		LeafValues.add(3);
+		LeafValues.add(4);
+		LeafValues.add(5);
+		LeafValues.add(6);
+		LeafValues.add(7);
+		LeafValues.add(8);
+		// Make sure there are 2^(depth-1) values in this array
+		
+
+		MiniMax minimax = new MiniMax();
+		
+		minimax.BuildTree(depth, LeafValues);
+		
+		minimax.SerachTree();
+    }
 	
-	MiniMax minimax = new MiniMax();
-	
-	minimax.BuildTree(depth, leafValues);
 }
